3daa690eb9bedd67d7ab8fc66c394c6afe9f308d
Kevin Kokal <39959677+kevinkokal@users.noreply.github.com>
2024-01-23 11:48:39 -0500

    Initial Commit

---
+++ PoKev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoKev
+//
+//  Created by Kevin Kokal on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoKev/PoKevApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoKevApp.swift
+//  PoKev
+//
+//  Created by Kevin Kokal on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PoKevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
